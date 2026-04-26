5d55d864ba3c8e4c3e6f87b47c7a14c59240d0e7
Andress Martin <andress.contato@gmail.com>
2026-04-26 17:15:55 -0300

    feat: add redirect from /vote to /pt-BR/picture-contest/voting

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
+  async redirects() {
+    return [
+      {
+        source: '/vote',
+        destination: '/pt-BR/picture-contest/voting',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
